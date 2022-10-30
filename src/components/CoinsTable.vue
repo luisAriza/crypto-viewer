8770ac7f281b88049a33042e5e2fda2726b2926b
Luis Ariza <luismanuelariza@hotmail.com>
2022-10-30 15:26:04 -0500

    [Upgrade] Formato de moneda y titulos de la tabla

--- src/components/CoinsTable.vue
+++ src/components/CoinsTable.vue
@@ -3,7 +3,7 @@
 	table
 		thead
 			tr
-				th.title(v-for="title in $store.state.titles" :key="title")
+				th.title(v-for="title in titles" :key="title")
 					span {{ title }}
 		tbody
 			tr(v-for="(coin, index) in $store.state.filteredCoins", :key="coin.id")
@@ -12,31 +12,40 @@
 					img.coin-img(:src="coin.image" :alt="coin.name" width="20" height="20")
 					span.coin-name {{ coin.name }}
 					span.coin-symbol {{ coin.symbol }}
-				td.price $ {{ coin.current_price }}
+				td.price {{ amountCurrency(coin.current_price) }}
 				td(:class="[coin.price_change_percentage_24h > 0 ? 'price-upper' : 'price-lower']")
 					span {{ coin.price_change_percentage_24h.toFixed(2) }}%
-				td.coin-volume $ {{ coin.total_volume.toLocaleString() }}
+				td.coin-volume {{ amountCurrency(coin.total_volume) }}
 </template>
 
 <script lang="ts">
-type Data = {
-  titles: string[];
-};
+	type Data = {
+		titles: string[];
+	};
+	const currencyFormatter = new Intl.NumberFormat("en-US", {
+		style: "currency",
+		currency: "USD",
+	});
 
-export default {
-  name: "CoinsTable",
-  data(): Data {
-    return {
-      titles: ["#", "coins", "price", "price change", "24h volume"],
-    };
-  },
-  async mounted(): Promise<void> {
-    const res = await fetch(
-      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
-    );
-    const data = await res.json();
-    this.$store.state.coins = data;
-    this.$store.state.filteredCoins = data;
-  },
-};
+	export default {
+		name: "CoinsTable",
+		data(): Data {
+			return {
+				titles: ["#", "coins", "price", "price change", "24h volume"],
+			};
+		},
+		async mounted(): Promise<void> {
+			const res = await fetch(
+				"https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+			);
+			const data = await res.json();
+			this.$store.state.coins = data;
+			this.$store.state.filteredCoins = data;
+		},
+		methods: {
+			amountCurrency(price: number): string {
+				return currencyFormatter.format(price);
+			},
+		},
+	};
 </script>
